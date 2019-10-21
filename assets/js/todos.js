35b9baf510b50e65864ed70180c258b8a11131c1
Eric Chen <cheneric817@gmail.com>
2019-10-22 00:14:28 +0800

    Basic function for deleting items
    Implemented the basic function for deleting todo items.

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -4,3 +4,12 @@
 $("li").click(function() {
   $(this).toggleClass("completed");
 });
+
+
+//click X to delete todo
+$("span").click(function(event) {
+  $(this).parent().fadeOut(500, function() {
+    $(this).remove(); //this refers to the <li>, not the <span> element
+  });
+  event.stopPropagation();
+});
